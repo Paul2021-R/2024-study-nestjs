982081b4f7376dfe4f38950fa4175df968d91e2e
hansol <ryu.axel@gmail.com>
2024-06-09 22:25:03 +0900

    adopt mongodb, prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [PrismaModule, TasksModule, UserModule],
   controllers: [],
   providers: [],
 })
